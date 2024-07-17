b0f42b5ddfa9501bf170046712e0e86bc7cc92df
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-07-17 14:49:34 +0500

    static site

--- src/Components/Main/HeroSection/HeroSection.tsx
+++ src/Components/Main/HeroSection/HeroSection.tsx
@@ -3,7 +3,7 @@ const HeroSection = () => {
     <>
       <section
         id="heroSection"
-        className="h-screen w-full before:content-[''] before:w-full before:h-full before:absolute before:top-0 before:left-0 border border-green-500 px-7 xl:px-20"
+        className="h-screen w-full border border-green-500 px-7 xl:px-20"
       ></section>
     </>
   );
