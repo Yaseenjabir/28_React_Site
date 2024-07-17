2d5ea4f164b9954d8ef1b1f8ae5e874640885065
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-07-17 14:46:08 +0500

    static site

--- src/Components/Main/HeroSection/HeroSection.tsx
+++ src/Components/Main/HeroSection/HeroSection.tsx
@@ -3,18 +3,8 @@ const HeroSection = () => {
     <>
       <section
         id="heroSection"
-        className="h-screen w-full before:content-[''] before:w-full before:h-full before:absolute before:top-0 before:left-0 flex flex-col-reverse text-end px-7 xl:px-20"
-      >
-        <div className="mb-8 z-10 w-full flex flex-col items-end md:mb-12 lg:mb-16 border">
-          <h1 className="text-black max-w-[800px] px-2 font-semibold leading-[50px] lg:text-6xl xl:text-7xl text-4xl">
-            Brandz Management Consultancy
-          </h1>
-          <p className="text-[25px] px-2 max-w-[1020px] lg:text-[29px]">
-            Connecting you to the world Fast and reliable telecommunications
-            services for your business needs
-          </p>
-        </div>
-      </section>
+        className="h-screen w-full before:content-[''] before:w-full before:h-full before:absolute before:top-0 before:left-0 border border-green-500 px-7 xl:px-20"
+      ></section>
     </>
   );
 };
